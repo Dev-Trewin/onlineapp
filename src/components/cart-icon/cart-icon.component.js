34ef745156e469baa98243027b902643103da731
Dev-Trewin <Roger.Trewin@fieldpro-software.com>
2023-12-04 19:31:26 -0700

    Added provider in Index.js
    Signed-off-by: Dev-Trewin <Roger.Trewin@fieldpro-software.com>

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -4,17 +4,16 @@ import { CartContext } from '../../context/cart.context';
 import './cart-icon.style.scss'
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
-  
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-  
-    return (
-      <div className='cart-icon-container' onClick={toggleIsCartOpen}>
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
-      </div>
-    );
-  };
-  
-  
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  console.log("value of isCartOpen :" + isCartOpen )
+  const toggleIsCartOpen = () => {setIsCartOpen(!isCartOpen)};
+
+  return (
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
+  );
+};
+
 export default CartIcon;
--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -6,39 +6,40 @@ import '../navigation/navegation.styles.scss';
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import {signOutUser} from '../../utils/firebase/firebase.utils';
 import { UserContext } from '../../context/user.context';
+import { CartContext } from '../../context/cart.context';
 
 const Navigation = () => {
     const {currentUser}=useContext(UserContext);
-    
+    const { isCartOpen } = useContext(CartContext);
+
 
 
     return (
         <Fragment>
-            <div className='navigation'>
-                <div className='logo-container'>
-                    <Link className='nav-link' to='/'>
-                       <CrwnLogo className='logo'/>
-                    </Link>
-                </div>
-                <div className='nav-links-container'>
-                    <Link className='nav-link' to='/shop'>
-                        SHOP
-                    </Link>
-                    {
-                       
-                        currentUser ? (
-                            <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
-                            ):(  <Link className='nav-link' to='/auth'>
-                                SIGN IN
-                               </Link>
-                        )
-                    }
-                  <CartIcon/>
-                </div>
-               <CartDropdown/>
-            </div>
-            <Outlet/>
-        </Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
     )
 
 };
