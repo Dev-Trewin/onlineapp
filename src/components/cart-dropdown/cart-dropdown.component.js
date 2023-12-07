6b8ab1bd834a6b7a9856b1dd5fdf565143283074
Dev-Trewin <Roger.Trewin@fieldpro-software.com>
2023-12-06 21:37:00 -0700

    Added  add to card functionality on shop page
    Signed-off-by: Dev-Trewin <Roger.Trewin@fieldpro-software.com>

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,26 +1,27 @@
-import Button from '../button/button.component';
+
 import { useContext } from 'react';
-import './cart-dropdown.styles.scss'
 import { CartContext } from '../../context/cart.context';
 import CartItem from '../../context/cart-item/cart-item.component';
+import Button from '../button/button.component';
+import './cart-dropdown.styles.scss';
 
-const CartDropdown=()=>{
-    const { cartItems } = useContext(CartContext);
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
 
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-items'>
+        {cartItems && cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
-    return (
-        <div className='cart-dropdown-container'>
-          <div className='cart-items'>
-            {cartItems.length ? (
-              cartItems.map((cartItem) => (
-                <CartItem key={cartItem.id} cartItem={cartItem} />
-              ))
-            ) : (
-              <span className='empty-message'>Your cart is empty</span>
-            )}
-          </div>
-          <Button>GO TO CHECKOUT</Button>
-        </div>
-      );
-    };
 export default CartDropdown;
--- src/context/cart.context.js
+++ src/context/cart.context.js
@@ -1,12 +1,34 @@
 import { createContext, useState } from 'react';
+// This function it will find if cartitems contains productToAdd
+ // if found , increment quantity
+ // return new  array with modified cartItems/new cart item
+const addCartItem=(cartItems,productToAdd)=>{
+  const existingCartItem = cartItems.find(
+    (cartItem)=>cartItem.id === productToAdd.id
+  );
+
+  if(existingCartItem){
+    return cartItems.map((cartItem)=>cartItem.id=== productToAdd.id ?// If we found item return a new array of a cartItems
+    { ...cartItem,quantity:cartItem.quantity +1} 
+    : cartItem//if this not the case just return the original cartItem
+    )
+  }
+  return [...cartItems,{...productToAdd,quantity:1}]
+}
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItem:[],
+  addItemToCart:()=>{}
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const  [cartItems,setCartItems]=useState([]);
+ 
+  const addItemToCart = (product) =>
+  setCartItems(addCartItem(cartItems, product));
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
