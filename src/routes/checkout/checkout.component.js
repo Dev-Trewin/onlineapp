ab6c271f03acbbb953618edf59f9b6e0794acf0a
Dev-Trewin <Roger.Trewin@fieldpro-software.com>
2023-12-18 19:28:32 -0700

    Added total function to checkout component
    Signed-off-by: Dev-Trewin <Roger.Trewin@fieldpro-software.com>

--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -5,8 +5,7 @@ import '../checkout/checkout.styles.scss'
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
+  const { cartItems,cartTotal } = useContext(CartContext);
   return (
     <div className="checkout-container">
       <div className="checkout-header">
@@ -32,7 +31,7 @@ const Checkout = () => {
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-        <span className="total">Total:0</span>
+        <span className="total">Total:{cartTotal}</span>
       </div>
    
   );
