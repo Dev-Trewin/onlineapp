dbead57d81e295556f8d5324f56aa3f83afa55e7
Dev-Trewin <Roger.Trewin@fieldpro-software.com>
2023-12-19 19:03:57 -0700

    Added fetch  data functionality from fire store
    Signed-off-by: Dev-Trewin <Roger.Trewin@fieldpro-software.com>

--- src/context/product.context.js
+++ src/context/product.context.js
@@ -1,18 +1,33 @@
-import PRODUCTS from '../shop-data.json';
+//import SHOP_DATA from "../shop-data";
 import { createContext, useEffect, useState } from "react";
-
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+//import {addCollectionAndDocuments}  from '../utils/firebase/firebase.utils';
 // This is the actual value that i want to access
 export const ProductContext=createContext({ 
   products:[],
 });
- 
-export const ProductProvider=({children})=>{
- const [products,setProducts]=useState(PRODUCTS);
- const value={products};
-return(
-  <ProductContext.Provider value={value}>
-    {children}
-  </ProductContext.Provider>
-);
-    
-}
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      try {
+        const categoryMap = await getCategoriesAndDocuments();
+        console.log(categoryMap);
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    getCategoriesMap();
+  }, []); // Empty dependency array indicates that this effect runs once on mount
+
+
+  const value = { products };
+  return (
+    <ProductContext.Provider value={value}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
