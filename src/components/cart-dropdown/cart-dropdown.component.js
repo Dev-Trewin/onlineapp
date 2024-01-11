b38d7c3592f3f1dd2a9a5f02f42d6811ccc622cc
Dev-Trewin <Roger.Trewin@fieldpro-software.com>
2024-01-11 15:54:38 -0700

    Added styles to navigation and form-input component
    Signed-off-by: Dev-Trewin <Roger.Trewin@fieldpro-software.com>

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -3,7 +3,11 @@ import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
-import './cart-dropdown.styles.scss';
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
 import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
@@ -15,18 +19,16 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems && cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
-          <span className='empty-message'>Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </div>
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
--- src/routes/navigation/navegation.styles.js
+++ src/routes/navigation/navegation.styles.js
@@ -1,28 +1,29 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const NavigationContainer =styled.div `
+export const NavigationContainer = styled.div`
   height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+`;
 
-`
-
-export const LogoContainer=styled(Link)`
+export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
-`
-export const NavLink=styled.div`
-  width: 100%;
+`;
+
+export const NavLinks = styled.div`
+  width: 50%;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: flex-end;
 `;
-export const NavLinks=styled.div`
+
+export const NavLink = styled(Link)`
   padding: 10px 15px;
   cursor: pointer;
 `;
